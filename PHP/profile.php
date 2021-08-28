f4916817dff3ca24d71f2adef53f345423129134
Koechian <ianovic8@live.com>
2021-08-28 14:09:56 +0300

    Customer clear history

--- PHP/profile.php
+++ PHP/profile.php
@@ -10,14 +10,21 @@ $tempid = $_SESSION['usr_id'];
 
 
 if (isset($_POST['logout'])) {
-
     session_destroy();
     header("location:../HTML\login.html");
 }
 
-
+if (isset($_POST['purge'])) {
+    $sql = "DELETE FROM confirmed WHERE id=$tempid";
+    if ($result = mysqli_query($link, $sql)) {
+        header('location:profile.php');
+    } else {
+        echo mysqli_error($link);
+    }
+}
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -52,45 +59,10 @@ if (isset($_POST['logout'])) {
             <div id="username-cont">
                 <img id="userimg" src="https://secure.gravatar.com/avatar/e9a1178888965fc58edcd11a70bff6f1?s=100&amp;d=mm" class="userpicture defaultuserpic" width="100" height="100" alt="Profile Picture">
 
-                <h1 id="username-disp"><?php echo $_SESSION['usr'] ?></h1>
-                <form action="" enctype="multipart/form-data" action="" method="POST">
+                <h1 id="username_disp"><?php echo $_SESSION['usr'] ?></h1>
+                <form action="" action="" method="POST">
                     <input name="logout" value="Logout" type="submit" id="logout-btn">
-                    <input name="upload" value="Upload" type="submit" id="upload-btn">
-                    <div id="uploads">
-                        <form name="uploadfile" enctype="multipart/form-data" action="" method="POST">
-                            <?php
-                            if (isset($_GET['upload'])) {
-                                echo <<<GFG
-                        <input type="file" name="fileToUpload" id="fileToUpload" />
-                        <input value="Upload" type="submit" name="img_upload"/>
-                        GFG;
-                                $msg = "";
-
-                                // If upload button is clicked ...
-                                if (isset($_POST['img_upload'])) {
-
-                                    $filename = $_FILES["uploadfile"]["name"];
-                                    $tempname = $_FILES["uploadfile"]["tmp_name"];
-                                    $folder = "../Assets/UserUploads/" . $filename;
-
-                                    // Get all the submitted data from the form
-                                    $sql = "UPDATE users SET pic='$filename' WHERE id =$temp";
-
-                                    // Execute query
-                                    mysqli_query($link, $sql);
-
-                                    // Now let's move the uploaded image into the folder: image
-                                    if (move_uploaded_file($tempname, $folder)) {
-                                        $msg = "Image uploaded successfully";
-                                    } else {
-                                        $msg = "Failed to upload image" . mysqli_error($link);
-                                    }
-                                    echo "<script>console.log('$msg')</script>";
-                                }
-                            }
-                            ?>
-                        </form>
-                    </div>
+                    <input name="purge" value="Clear History" type="submit" id="upload-btn">
                 </form>
 
             </div>
